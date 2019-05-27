98763f5da981fb5001605dac07e532afd02482a4
ShadowRonin <shaodw_ronin@gmx.com>
2019-05-27 15:31:19 -0400

    Updated filter layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,120 +1,7 @@
 <template>
   <div class="w-full flex justify-center">
     <div class="w-3/4">
-      <div class="filter mt-3 p-2 rounded bg-primary text-primary-text">
-        <button @click="showFilter = !showFilter">
-          <i class="fas fa-filter" title="Filter"></i>
-        </button>
-        <div v-if="showFilter" class="inline-block mx-1">
-          <button @click="clearFilter()">
-            <i class="fas fa-times"></i>
-            Clear Filter
-          </button>
-          <div class="search">
-            Search:
-            <input
-              class="rounded text-secondary-text text-center"
-              type="text"
-              v-model="filter.search"
-              @input="filterApplications()"
-            >
-          </div>
-          <div class="experience my-2">
-            Experience range:
-            <input
-              class="w-5 rounded text-secondary-text text-center"
-              type="number"
-              v-model="filter.experience.minimum"
-              name="minimumExperience"
-              @input="filterApplications()"
-            >
-            -
-            <input
-              class="w-5 rounded text-secondary-text text-center"
-              type="number"
-              v-model="filter.experience.maximum"
-              name="maximumExperience"
-              @input="filterApplications()"
-            >
-          </div>
-          <div class="applied my-2">
-            Applied:
-            <Datepicker
-              class="datepicker text-secondary-text"
-              wrapper-class="inline-datepicker"
-              input-class="rounded"
-              v-model="filter.applied.start"
-              placeholder="Start Date"
-              @closed="filterApplications()"
-            />
-            <Datepicker
-              class="datepicker text-secondary-text"
-              wrapper-class="inline-datepicker"
-              input-class="rounded"
-              v-model="filter.applied.end"
-              placeholder="End Date"
-              @closed="filterApplications()"
-            />
-          </div>
-          <div class="availability">
-            Availability:
-            <DayOfWeekCheckbox
-              v-model="filter.availability.Su"
-              day="S"
-              title="Availability Sunday"
-              @change="filterApplications()"
-            />
-            <DayOfWeekCheckbox v-model="filter.availability.M" day="M" title="Availability Monday"/>
-            <DayOfWeekCheckbox
-              v-model="filter.availability.T"
-              day="T"
-              title="Availability Tuesday"
-              @change="filterApplications()"
-            />
-            <DayOfWeekCheckbox
-              v-model="filter.availability.W"
-              day="W"
-              title="Availability Wednesday"
-              @change="filterApplications()"
-            />
-            <DayOfWeekCheckbox
-              v-model="filter.availability.Th"
-              day="T"
-              title="Availability Thursday"
-              @change="filterApplications()"
-            />
-            <DayOfWeekCheckbox
-              v-model="filter.availability.F"
-              day="F"
-              title="Availability Friday"
-              @change="filterApplications()"
-            />
-            <DayOfWeekCheckbox
-              v-model="filter.availability.S"
-              day="S"
-              title="Availability Saturday"
-              @change="filterApplications()"
-            />
-          </div>
-          <div class="bookmarked">
-            Bookmark:
-            <Checkbox
-              class="text-sm mr-2"
-              v-model="filter.bookmark.showBookmarked"
-              label="Bookmarked"
-              @change="filterApplications()"
-            />
-            <Checkbox
-              class="text-sm"
-              v-model="filter.bookmark.showUnBookmarked"
-              label="Not Bookmarked"
-              @change="filterApplications()"
-            />
-          </div>
-        </div>
-      </div>
-
-      <div class="sort inline-block mt-3 p-2 rounded bg-primary text-primary-text">
+      <div class="sort align-top inline-block mt-3 mr-3 p-2 rounded bg-primary text-primary-text">
         Sort By:
         <select
           class="sort-select cursor-pointer bg-primary text-primary-text font-semibold mr-1"
@@ -126,7 +13,7 @@
           <option value="position">Position</option>
           <option value="applied">Applied</option>
           <option value="experience">Experience</option>
-          <option value="availability">Total Availability</option>
+          <option value="availability">Availability</option>
           <option value="bookmarked">Bookmarked</option>
         </select>
         <button @click="toggleSortDirection()">
@@ -136,6 +23,127 @@
             :class="{'fas fa-sort-amount-up': sortAscending, 'fas fa-sort-amount-down': !sortAscending}"
           ></i>
         </button>
+      </div>
+      <div class="filter inline-block mt-3 p-2 rounded bg-primary text-primary-text">
+        <div class="flex align-top justify-start items-start">
+          <button
+            @click="showFilter = !showFilter"
+            :title="showFilter? 'Hide Filter': 'Show Filter'"
+          >
+            <i class="fas fa-filter"></i>
+          </button>
+          <div v-if="showFilter" class="flex flex-col flex-grow mx-2 items-start">
+            <button class="self-end" @click="clearFilter()">
+              <i class="fas fa-times"></i>
+              Clear Filter
+            </button>
+            <div class="search">
+              Search:
+              <input
+                class="rounded text-secondary-text text-center"
+                type="text"
+                v-model="filter.search"
+                @input="filterApplications()"
+              >
+            </div>
+            <div class="experience my-2">
+              Experience range:
+              <input
+                class="w-5 rounded text-secondary-text text-center"
+                type="number"
+                v-model="filter.experience.minimum"
+                name="minimumExperience"
+                @input="filterApplications()"
+              >
+              -
+              <input
+                class="w-5 rounded text-secondary-text text-center"
+                type="number"
+                v-model="filter.experience.maximum"
+                name="maximumExperience"
+                @input="filterApplications()"
+              >
+            </div>
+            <div class="applied my-2">
+              Applied:
+              <Datepicker
+                class="datepicker text-secondary-text"
+                wrapper-class="inline-datepicker"
+                input-class="rounded"
+                v-model="filter.applied.start"
+                placeholder="Start Date"
+                @closed="filterApplications()"
+              />
+              <Datepicker
+                class="datepicker text-secondary-text"
+                wrapper-class="inline-datepicker"
+                input-class="rounded"
+                v-model="filter.applied.end"
+                placeholder="End Date"
+                @closed="filterApplications()"
+              />
+            </div>
+            <div class="availability">
+              Availability:
+              <DayOfWeekCheckbox
+                v-model="filter.availability.Su"
+                day="S"
+                title="Availability Sunday"
+                @change="filterApplications()"
+              />
+              <DayOfWeekCheckbox
+                v-model="filter.availability.M"
+                day="M"
+                title="Availability Monday"
+              />
+              <DayOfWeekCheckbox
+                v-model="filter.availability.T"
+                day="T"
+                title="Availability Tuesday"
+                @change="filterApplications()"
+              />
+              <DayOfWeekCheckbox
+                v-model="filter.availability.W"
+                day="W"
+                title="Availability Wednesday"
+                @change="filterApplications()"
+              />
+              <DayOfWeekCheckbox
+                v-model="filter.availability.Th"
+                day="T"
+                title="Availability Thursday"
+                @change="filterApplications()"
+              />
+              <DayOfWeekCheckbox
+                v-model="filter.availability.F"
+                day="F"
+                title="Availability Friday"
+                @change="filterApplications()"
+              />
+              <DayOfWeekCheckbox
+                v-model="filter.availability.S"
+                day="S"
+                title="Availability Saturday"
+                @change="filterApplications()"
+              />
+            </div>
+            <div class="bookmarked">
+              Bookmark:
+              <Checkbox
+                class="text-sm mr-2"
+                v-model="filter.bookmark.showBookmarked"
+                label="Bookmarked"
+                @change="filterApplications()"
+              />
+              <Checkbox
+                class="text-sm"
+                v-model="filter.bookmark.showUnBookmarked"
+                label="Not Bookmarked"
+                @change="filterApplications()"
+              />
+            </div>
+          </div>
+        </div>
       </div>
 
       <ApplicationPreview
@@ -317,7 +325,6 @@ export default {
   /* Remove select's arrow */
   appearance: none;
 }
-
 /* Remove arrows from input */
 .experience input[type="number"]::-webkit-outer-spin-button,
 .experience input[type="number"]::-webkit-inner-spin-button {
